8922a3acedf4b5c0dff64fe0e4e53fbc355f72d4
victo <victo@Victor>
2017-05-26 16:47:34 -0300

    arrea administrativa

---
+++ restrito/editarR.php
@@ -0,0 +1,29 @@
+<?php
+include('../repositorio/Master.php');
+$id = (int) $_GET['id'];
+
+if(isset($_POST['valor'])){
+
+   $valor = $_POST['valor'];
+
+   if($result=$conn->query("UPDATE receita SET valor = '{$valor}' WHERE idreceita='{$id}';")){
+   }
+
+}
+
+if($result=$conn->query("SELECT aplicacao_idaplicacao,fonte_recurso_idfonte_recurso,fonte_idfonte,data,valor FROM `receita` WHERE idreceita='{$id}'")){
+    if($result){
+        while($row = $result->fetch_assoc()){
+            $array[]=$row;
+        }
+    }
+    $result->close();
+}
+?>
+<form action="#" method="post">
+
+   <label>valor</label>
+    <input type="text" name="valor" value="<?php echo $array[0]['valor']; ?>">
+    <button type="input">Alterar</button>
+
+</form>
